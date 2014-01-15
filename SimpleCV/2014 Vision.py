e876667f111ce95c21e7595264c41b3f463b020b
Frankie Lyon <frankthelyon@gmail.com>
2014-01-14 17:48:48 -0800

    Changed file folder name.

---
+++ SimpleCV/2014 Vision.py
@@ -0,0 +1,29 @@
+import os
+import glob
+import copy
+import time
+import re
+from SimpleCV import *
+from socket import *
+cam = Camera()
+myPort = 1025
+myHost = ''
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((myHost, myPort))
+s.listen(5)
+i = 0
+while True:
+	img = cam.getImage()
+	blobs = img.findBlobs()
+	#test = [b for b in blobs if b.isRectangle(tolerance=0.015)]
+	test = FeatureSet([b for b in blobs if b.isRectangle(tolerance=0.045) and b.width() > b.height() + 40])
+	test.draw(color=Color.RED,width=4)
+	img.drawText(str(test.coordinates()))
+	img.show()
+	if i < 10:
+		pass
+	else:
+		connection, address = s.accept()
+		connection.send(test)
+		i = 0
+	
