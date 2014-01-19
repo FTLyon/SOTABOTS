b6bd9fab2217cab2fd72bafd78f9a0ffc04426cc
Frankie Lyon <frankthelyon@gmail.com>
2014-01-18 16:27:00 -0800

    Cleaned up Vision.java methods.

--- JAVA 2014/src/edu/wpi/first/wpilibj/templates/Network.java
+++ JAVA 2014/src/edu/wpi/first/wpilibj/templates/Network.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.Timer;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.Byte;
 import javax.microedition.io.Connector;
 import javax.microedition.io.SocketConnection;
 
