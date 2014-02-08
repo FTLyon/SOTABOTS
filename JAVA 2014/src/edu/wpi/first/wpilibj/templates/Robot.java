042171cd2ade3586c6e8887041e9e3450397831e
Frankie <frankthelyon@gmail.com>
2014-02-08 13:22:39 -0800

    Network in, LED manual mode control

--- JAVA 2014/src/edu/wpi/first/wpilibj/templates/Robot.java
+++ JAVA 2014/src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -1,9 +1,9 @@
 package edu.wpi.first.wpilibj.templates;
 
-
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 
 
-public class Robot extends SimpleRobot {
+public class RobotTemplate extends SimpleRobot {
     Joystick   leftStick            = new Joystick(1);
     Joystick   rightStick           = new Joystick(2);
     RobotDrive drive                = new RobotDrive(1,2);
@@ -26,81 +26,147 @@ public class Robot extends SimpleRobot {
     Solenoid   shift_2              = new Solenoid(4);
     Solenoid   lock_1               = new Solenoid(5);
     Solenoid   lock_2               = new Solenoid(6);
+    Encoder    winchEncoder         = new Encoder(1,2);
+    DigitalOutput mode_1            = new DigitalOutput(1);
+    DigitalOutput mode_2            = new DigitalOutput(2);
+    DigitalOutput mode_3            = new DigitalOutput(3);
+    DigitalOutput mode_groovy       = new DigitalOutput(4);
     Timer      time_1               = new Timer();
     
-    boolean    intakeDown           = false;
+    boolean    intakeDown           = true;
     boolean    intake               = false;
     boolean    winchDown            = false;
     boolean    shifted              = false;
     boolean    locked               = false;
+    boolean    shoot_1              = false;
+    boolean    shoot_2              = false;
+    boolean    shoot_3              = false;
     
     double     driveLeft            = 0;
     double     driveRight           = 0;
     
    
     public void autonomous() {
-        
+        mode_1.set(true);
     }
 
     public void operatorControl() {
         compressor.start();
-        
-        while (isOperatorControl() && isEnabled()) {
-            if (Math.abs(leftStick.getAxis(Joystick.AxisType.kX)) > 0.15)           
-                drive.arcadeDrive(leftStick);
-            else if (Math.abs(leftStick.getAxis(Joystick.AxisType.kY)) > 0.15)
-                drive.arcadeDrive(leftStick);
-            else
-                drive.arcadeDrive(0,0);
+        winchEncoder.start();
+        while (isOperatorControl() && isEnabled()) 
+            Network.NetIn();
+            
+            if (Math.abs(leftStick.getAxis(Joystick.AxisType.kX)) > 0.15)   {        
+                drive.arcadeDrive(leftStick);}
+            else if (Math.abs(leftStick.getAxis(Joystick.AxisType.kY)) > 0.15) {
+                drive.arcadeDrive(leftStick);}
+            else{
+                drive.arcadeDrive(0,0);}
             wench.set(rightStick.getAxis(Joystick.AxisType.kY));
+            
                        
-/*left*/    if (leftStick.getRawButton(6)) 
-                shifted = true;
-            else if (leftStick.getRawButton(7)) 
-                shifted = false;
+/*left*/    if (leftStick.getRawButton(6)) {
+                shifted = true;}
+            else if (leftStick.getRawButton(7)) {
+                shifted = false;}
             
-/*right*/   if (rightStick.getRawButton(3)) 
-                winchDown = true;
+            if (leftStick.getRawButton(3)) {
+                intakeDown = true;
+            }
+            else if (leftStick.getRawButton(2)) {
+                intakeDown = false;
+            }
             
-            if (rightStick.getRawButton(7) && intake == false)
-                intake    = true;
-            else if (rightStick.getRawButton(7) && intake == true) 
-                intake    = false;
-            
-            if (rightStick.getRawButton(2) && rightStick.getTrigger()) 
-                locked = false;
-            
-////////////////////////////////////////////////////////////////////////////////
-            
-/*winch*/   if (winchDown = true && lim_switch.get() == false) 
-                wench.set(.4);            
-            else if (lim_switch.get()) 
-                wench.set(rightStick.getAxis(Joystick.AxisType.kY));
+/*right*/   if (rightStick.getRawButton(3)) {
+                winchDown = true;}
+            else {
                 winchDown = false;
-                locked = true;
-            
-/*lock*/    if (locked) {
-                lock_1.set(true);
-                lock_2.set(false);}
-            else if (locked == false) 
+            }           
+            if (rightStick.getRawButton(7)) {
+                intake = true;
+            }
+            else {
+                intake = false;
+            }
+             
+            if (rightStick.getTrigger() && rightStick.getRawButton(9)) {
+                locked = false;
+                winchDown = false;}
+            else {
                 lock_1.set(false);
                 lock_2.set(true);
+            }
             
-/*intake-m*/if (intake) 
-                intakeMotor.set(1);
-            else if (intake == false) 
-                intakeMotor.set(0);
+/*groovy*/  if (rightStick.getRawButton(4)) { //button 4
+                mode_1.set(false);
+                mode_2.set(false);
+                mode_3.set(false);
+                mode_groovy.set(true);
+            }
             
+/*mode2*/   if (rightStick.getRawButton(5)) { //button 5
+                mode_1.set(false);
+                mode_2.set(true);
+                mode_3.set(false);
+                mode_groovy.set(false);
+            }
+
+/*mode3*/   if (rightStick.getRawButton(9)) { //button 9
+                mode_1.set(false);
+                mode_2.set(false);
+                mode_3.set(true);
+                mode_groovy.set(false);
+            }
+                        
+////////////////////////////////////////////////////////////////////////////////
+            
+/*winch*/   if (winchDown = true && lim_switch.get() == true && rightStick.getRawButton(0) == false && rightStick.getRawButton(3)) {
+                wench.set(-.4);
+                locked = false;}
+            else if (lim_switch.get() == false && rightStick.getRawButton(0) == false && winchDown == true) {
+                wench.set(rightStick.getAxis(Joystick.AxisType.kY));
+                //winchDown = false;
+                locked = true;}
+            else if (lim_switch.get() == false && rightStick.getRawButton(3)) {
+                locked = true;
+            }
+            
+            
+            
+/*lock*/    if (locked) {
+                lock_1.set(false);
+                lock_2.set(true);}
+            else if (locked == false) {
+                winchEncoder.reset();
+                winchEncoder.start();
+                System.out.println(winchEncoder.get());
+                lock_1.set(true);
+                lock_2.set(false);
+                }
+            
+/*intake-m*/if (intake) {
+                intakeMotor.set(1);}
+            else if (intake == false) {
+                intakeMotor.set(0);}
+/*intake-down*/
+            if (intakeDown) {
+                intake_1.set(true);
+                intake_2.set(false);
+            }
+            else {
+                intake_1.set(false);
+                intake_2.set(true);
+            }
 /*shifters*/if (shifted) {
                 shift_1.set(true);
                 shift_2.set(false);}
-            else 
+            else {
                 shift_1.set(false);
-                shift_2.set(true);
+                shift_2.set(true);}
             
             Timer.delay(.01);
             
-        }
+        
     }
     
     public void test() {
