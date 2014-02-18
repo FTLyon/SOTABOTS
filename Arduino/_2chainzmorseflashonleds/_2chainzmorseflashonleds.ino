7a596d63a2e0fd9158116b00193a417233b5d72a
Preston Holmgren <guitarpreston>
2014-02-18 15:03:14 -0800

    Revert "Here ya' go!"
    This reverts commit 6089e81021866a71a6e8609d289ba39e6c2aab0d.

--- Arduino/_2chainzmorseflashonleds/_2chainzmorseflashonleds.ino
+++ Arduino/_2chainzmorseflashonleds/_2chainzmorseflashonleds.ino
@@ -1,41 +1,36 @@
 #include <Adafruit_NeoPixel.h>
-int leds = 8;
-int port = 7;
-#define PIN 6
-
-#define NUM_LIGHTS  8
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(leds, port, NEO_GRB + NEO_KHZ800);
-void setup(){
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, 7, NEO_GRB + NEO_KHZ800);
+void setup(){                          //#leds port
   strip.begin();//prepares strip
   strip.show();
 }
 void loop(){
-  dotpartspace();            /*2*/
+  dotpartspace();  //2
   dotpartspace();
   dashpartspace();
   dashpartspace();
   dashletterspace();
   
-  dashpartspace();           /*c*/
+  dashpartspace();  //c
   dotpartspace();
   dashpartspace();
   dotletterspace();
   
-  dotpartspace();            /*h*/
+  dotpartspace();    //h
   dotpartspace();
   dotpartspace();
   dotletterspace();
   
-  dotpartspace();            /*a*/
+  dotpartspace();    //a
   dashletterspace();
   
-  dotpartspace();            /*i*/
+  dotpartspace();    //i
   dotletterspace();
   
-  dashpartspace();           /*n*/
+  dashpartspace();    //n
   dotletterspace();
   
-  dashpartspace();           /*z*/
+  dashpartspace();    //z
   dashpartspace();
   dotpartspace();
   dotspace();
@@ -43,42 +38,42 @@ void loop(){
 }
 void dotpartspace(){
   strip.Color(235, 255, 0), 50;
-  strip.setPixelColor(leds, port);
+  strip.setPixelColor(8, 7);
   delay(63);
   strip.show();
   delay(63);
 }
 void dashpartspace(){
   strip.Color(235, 255, 0), 50;
-  strip.setPixelColor(leds, port);
+  strip.setPixelColor(8, 7);
   delay(188);
   strip.show();
   delay(63);
 }
 void dotletterspace(){
   strip.Color(235, 255, 0), 50;
-  strip.setPixelColor(leds, port);
+  strip.setPixelColor(8, 7);
   delay(63);
   strip.show();
   delay(188);
 }
 void dashletterspace(){
   strip.Color(235, 255, 0), 50;
-  strip.setPixelColor(leds, port);
+  strip.setPixelColor(8, 7);
   delay(188);
   strip.show();
   delay(188);
 }
 void dotspace(){
   strip.Color(235, 255, 0), 50;
-  strip.setPixelColor(leds, port);
+  strip.setPixelColor(8, 7);
   delay(63);
   strip.show();
   delay(438);
 }
 void dashspace(){
   strip.Color(235, 255, 0), 50;
-  strip.setPixelColor(leds, port);
+  strip.setPixelColor(8, 7);
   delay(188);
   strip.show();
   delay(438);
