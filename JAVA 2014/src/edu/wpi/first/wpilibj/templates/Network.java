290ee5a912852e0204e79e412030ce60af3deefe
EmperoroftheNorth <areichnorris@gmail.com>
2014-03-09 16:08:52 -0700

    Changed Network
    Crio connects to raspberry pi port 111 over Ethernet and listens for Boolean of hot goal detection.
    Also I made an extra function to pick up coordinates in a string format, from the pi in the same way.
    Please leave comment if clarification is need. I will put in comments.

--- JAVA 2014/src/edu/wpi/first/wpilibj/templates/Network.java
+++ JAVA 2014/src/edu/wpi/first/wpilibj/templates/Network.java
@@ -1,64 +1,77 @@
-package edu.wpi.first.wpilibj.templates;
+/*
+Error code 221: Socket could not be opened.
+Error code 222: Data stream could not be created.
+Error code 223: Could not read boolean.
+Error code 224: Could not read String.
+*/
 
-import com.sun.squawk.io.BufferedReader;
-import edu.wpi.first.wpilibj.Timer;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Vector;
+package edu.wpi.first.wpilibj.templates;
 import javax.microedition.io.Connector;
 import javax.microedition.io.SocketConnection;
+import java.io.IOException;
+import java.io.DataInput;
 
-/**
- *
- * @author Frankie Lyon
- */
 public class Network {
-    
-    public static String[] NetIn() {
-        int i = 0;
-        String[] xy = new String[]{"-1","-1","-1","-1"};
-        try {
-            //Network input from Raspberry pi
-            SocketConnection sc = (SocketConnection)Connector.open("socket://10.25.57.10:111");
-            sc.setSocketOption(SocketConnection.LINGER, 1);
-            InputStream is = sc.openInputStream();
-            BufferedReader in = new BufferedReader(new InputStreamReader(is));
-            xy = Split(in.readLine().toString(), " ");
-            System.out.println(xy);
-            Timer.delay(.01);
-        } catch (IOException ex) {
-            System.out.println("Recieve data failed.");
-            Timer.delay(.1);
+    public int Listenbool(){
+        //If error occurs returns value -1
+        //If no error occurs returns 1(true) or 0(false)
+        SocketConnection socket;
+        DataInput data;
+        int seen = -1;
+        try{
+            socket = (SocketConnection) Connector.open("10.25.57.10:111");
         }
-        return xy;    
+        catch(IOException e){
+            System.out.println("Error code 221");
+            return seen;
+        }
+        try{
+            data = socket.openDataInputStream();
+        }
+        catch(IOException e){
+            System.out.println("Error code 222");
+            return seen;
+        }
+        try{
+            if(data.readBoolean()){
+                seen = 1;
+            }
+            else{
+                seen = 0;
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error code 223");
+            return -1;
+        }
+        return seen;
     }
-    
-    public static String[] Split(String splitStr, String delimiter) {  
-    StringBuffer token = new StringBuffer();  
-    Vector tokens = new Vector();  
-    // split  
-    char[] chars = splitStr.toCharArray();  
-    for (int i=0; i < chars.length; i++) {  
-        if (delimiter.indexOf(chars[i]) != -1) {  
-            // we bumbed into a delimiter  
-            if (token.length() > 0) {  
-                tokens.addElement(token.toString());  
-                token.setLength(0);  
-            }  
-        } else {  
-            token.append(chars[i]);  
-        }  
-    }  
-    // don't forget the "tail"...  
-    if (token.length() > 0) {  
-        tokens.addElement(token.toString());  
-    }  
-    // convert the vector into an array  
-    String[] splitArray = new String[tokens.size()];  
-    for (int i=0; i < splitArray.length; i++) {  
-        splitArray[i] = (String) tokens.elementAt(i);  
-    }  
-    return splitArray;  
-}  
+    public String ListenCoor(){
+        //If error ocurrs returns value "-2,-2"
+        //If no error occurs returns "x,y" or "-1,-1" if nothing found
+        SocketConnection socket;
+        DataInput data;
+        String seen = "-2,-2";
+        try{
+            socket = (SocketConnection) Connector.open("10.25.57.10:111");
+        }
+        catch(IOException e){
+            System.out.println("Error code 221");
+            return seen;
+        }
+        try{
+            data = socket.openDataInputStream();
+        }
+        catch(IOException e){
+            System.out.println("Error code 222");
+            return seen;
+        }
+        try{
+            return data.readUTF();
+        }
+        catch(IOException e){
+            System.out.println("Error code 224");
+            return "-2,-2";
+        }
+    }
 }
