432c2f65067ffab5be4a15606b688bd007fb02c5
Frankie Lyon <frankthelyon@gmail.com>
2014-03-19 13:14:42 -0700

    Added excel python script
    Scout.py creates and edits excel files. The user must have xlwt, xlrd,
    and xlutils installed on their computer to run this script.

---
+++ Scout.py
@@ -0,0 +1,40 @@
+from xlwt import *
+from xlutils.copy import copy
+from xlrd import *
+from time import sleep
+
+inputs = ['Match','Auto Hot','Auto High','Auto Low','Auto Zone','Tele Pass','Tele Score','Tele Truss']
+
+def init(name):
+	file = name + '.xls'
+	try:
+		w = copy(open_workbook(file))
+	except:
+		w = Workbook()
+		ws = w.add_sheet(name)
+		for i in range(1,50):
+			ws.write(i,0,str(i))
+		for n in range(0,len(inputs)):
+			ws.write(0,n,inputs[n])
+		w.save(file)
+	print("Initialized...")
+
+def write(name,match,data):
+	file = name + '.xls'
+	try:
+		w = copy(open_workbook(file))
+	except:
+		init(name)
+		w = copy(open_workbook(file))
+	row = match
+	col = 1
+	for d in data:
+		w.get_sheet(0).write(row,col,str(d))
+		col += 1
+	w.save(file)
+	print("Data written to " + file)
+
+#init("2557")
+#write(1,(1,3,5,13,26))
+print("Yep")
+sleep(2)
